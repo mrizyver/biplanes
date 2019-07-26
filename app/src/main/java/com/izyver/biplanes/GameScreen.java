b0a3acedbee552bcf86e81a592f28308e7c147cf
Izyver <mr.izyver@gmail.com>
2019-07-27 00:40:43 +0300

    added engine module

---
+++ app/src/main/java/com/izyver/biplanes/GameScreen.java
@@ -0,0 +1,26 @@
+package com.izyver.biplanes;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.annotation.Nullable;
+
+public class GameScreen extends View {
+    public GameScreen(Context context) {
+        super(context);
+    }
+
+    public GameScreen(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameScreen(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public GameScreen(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+}
